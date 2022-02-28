f8df598d2e996fefd5eedd5a6a4cbeff77ae378f
Hayden Brown <haydenbrowncinci@gmail.com>
2022-02-28 14:24:58 -0500

    Add project files.

---
+++ FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FutureValue.Models;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FutureValue = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel fv)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FutureValue = fv.Calculate().ToString("c2");
+            }
+            else
+            {
+                ViewBag.FutureValue = "";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ FutureValue/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FutureValue
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
